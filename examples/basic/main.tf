fc688ab15ea9d301bb43ee3376587277d7b52a42
Valerio Bontempi <valerio.bontempi@it.ibm.com>
2026-02-27 22:40:21 +0100

    fix: fixed deps

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -4,7 +4,7 @@
 
 module "resource_group" {
   source  = "terraform-ibm-modules/resource-group/ibm"
-  version = "1.4.6"
+  version = "1.4.8"
   # if an existing resource group is not set (null) create a new one using prefix
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
@@ -74,7 +74,7 @@ locals {
 
 module "ocp_base" {
   source                              = "terraform-ibm-modules/base-ocp-vpc/ibm"
-  version                             = "3.71.4"
+  version                             = "3.81.5"
   resource_group_id                   = module.resource_group.resource_group_id
   region                              = var.region
   tags                                = var.resource_tags
