b30116703c39321c9be9a83404e93f94b96e047a
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-05-01 21:52:00 +0000

    chore(deps): update terraform terraform-ibm-modules/base-ocp-vpc/ibm to v3.87.0

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -74,7 +74,7 @@ locals {
 
 module "ocp_base" {
   source                              = "terraform-ibm-modules/base-ocp-vpc/ibm"
-  version                             = "3.86.0"
+  version                             = "3.87.0"
   resource_group_id                   = module.resource_group.resource_group_id
   region                              = var.region
   tags                                = var.resource_tags
