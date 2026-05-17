2253d6c02fee0f6de52e968a029ac45abe58d80a
raagamounica <mounica.tellabati@ibm.com>
2026-05-17 17:27:19 +0530

    chore: Add proxy_metadata in all examples for consistency

--- examples/existing_cluster/variables.tf
+++ examples/existing_cluster/variables.tf
@@ -59,3 +59,9 @@ variable "istio_controlplane_namespace" {
   default     = "istio-system-v3"
   nullable    = false
 }
+
+variable "proxy_metadata" {
+  description = "Additional key-value pairs to merge into meshConfig.defaultConfig.proxyMetadata for the Istio control plane. Useful for HTTP_PROXY, HTTPS_PROXY, etc."
+  type        = map(string)
+  default     = {}
+}
